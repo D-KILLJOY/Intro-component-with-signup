6ab277f564331a24ba9f82c4a42fc427e9a760c9
Dev KILLJOY <didiauche@gmail.com>
2025-12-17 18:50:14 +0100

    added error styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,25 @@
+import { useState } from "react";
+import errIcon from "./assets/images/icon-error.svg";
+
 function App() {
+    const [inputStatus, setInputStatus] = useState({
+        firstName: { value: "", error: false },
+        lastName: { value: "", error: false },
+        email: { value: "", error: false },
+        password: { value: "", error: false },
+    });
+
+    console.log(inputStatus.firstName);
+
+    function formValidate(e: any) {
+        e.preventDefault();
+
+        const form = e.target;
+        const formData = new FormData(form);
+        const formJson = Object.fromEntries(formData.entries());
+        console.log(formJson);
+    }
+
     return (
         <main className="bg-[url('./assets/images/bg-intro-mobile.png')] bg-c-Red-400 w-full min-h-screen flex justify-center items-center px-6 py-24">
             <div>
@@ -22,24 +43,129 @@ function App() {
                             then $20/mo. thereafter
                         </p>
                     </div>
-                    <form className="bg-white p-6 rounded-xl flex flex-col gap-4 shadow-p-shadow">
-                        <div>
-                            <input type="text" placeholder="First Name" />
-                            {
-                                <p className="text-right font-poppins-600 italic text-c-Red-400/80 text-xs mt-2">
+                    <form
+                        onSubmit={formValidate}
+                        className="bg-white p-6 rounded-xl flex flex-col gap-4 shadow-p-shadow"
+                    >
+                        <div className="relative">
+                            <input
+                                name="firstName"
+                                className={
+                                    inputStatus.firstName.error === true
+                                        ? "input__err"
+                                        : " "
+                                }
+                                type="text"
+                                placeholder={
+                                    inputStatus.firstName.error === true
+                                        ? ""
+                                        : "First Name"
+                                }
+                            />
+
+                            {inputStatus.firstName.error === true && (
+                                <img
+                                    src={errIcon}
+                                    className="absolute bottom-1/2 right-3"
+                                    alt=""
+                                />
+                            )}
+                            {inputStatus.firstName.error === true && (
+                                <p className="error__msg">
                                     {" "}
                                     First Name cannot be empty
                                 </p>
-                            }
+                            )}
                         </div>
-                        <div>
-                            <input type="text" placeholder="Last Name" />
+                        <div className="relative">
+                            <input
+                                name="lastName"
+                                className={
+                                    inputStatus.lastName.error === true
+                                        ? "input__err"
+                                        : " "
+                                }
+                                type="text"
+                                placeholder={
+                                    inputStatus.lastName.error
+                                        ? ""
+                                        : "Last Name"
+                                }
+                            />
+                            {inputStatus.lastName.error === true && (
+                                <img
+                                    src={errIcon}
+                                    className="absolute bottom-1/2 right-3"
+                                    alt=""
+                                />
+                            )}
+                            {inputStatus.lastName.error === true && (
+                                <p className="error__msg">
+                                    {" "}
+                                    Last Name cannot be empty
+                                </p>
+                            )}
                         </div>
-
-                        <input type="email" placeholder="Email Address" />
-                        <input type="password" placeholder="Password" />
+                        <div className="relative">
+                            <input
+                                name="email"
+                                className={
+                                    inputStatus.email.error === true
+                                        ? "input__err"
+                                        : " "
+                                }
+                                type="email"
+                                placeholder={
+                                    inputStatus.email.error === true
+                                        ? ""
+                                        : "Email Address"
+                                }
+                            />
+                            {inputStatus.email.error === true && (
+                                <img
+                                    src={errIcon}
+                                    className="absolute bottom-1/2 right-3"
+                                    alt=""
+                                />
+                            )}
+                            {inputStatus.email.error === true && (
+                                <p className="error__msg">
+                                    {" "}
+                                    Looks like this is not an email
+                                </p>
+                            )}
+                        </div>
+                        <div className="relative">
+                            <input
+                                name="password"
+                                className={
+                                    inputStatus.password.error === true
+                                        ? "input__err"
+                                        : " "
+                                }
+                                type="password"
+                                placeholder={
+                                    inputStatus.password.error === true
+                                        ? ""
+                                        : "Password"
+                                }
+                            />
+                            {inputStatus.firstName.error === true && (
+                                <img
+                                    src={errIcon}
+                                    className="absolute bottom-1/2 right-3"
+                                    alt=""
+                                />
+                            )}
+                            {inputStatus.firstName.error === true && (
+                                <p className="error__msg">
+                                    {" "}
+                                    Password cannot be empty
+                                </p>
+                            )}
+                        </div>
                         <button
-                            type="button"
+                            type="submit"
                             className="border-c-Green-400 bg-c-Green-400 text-white uppercase font-poppins-500 py-4 rounded-lg shadow-g-shadow hover:bg-c-Green-400/70 cursor-pointer"
                         >
                             Claim your free trial
